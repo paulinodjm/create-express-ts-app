69e33bad9a0b457e4c252ef4ddf23ba78bc2c214
Paulino <paulinodjm@hotmail.com>
2018-01-17 11:15:12 +0100

    Add the parameter check

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
 #!/usr/bin/env node
-console.log("Hello world!")
+
+const path = require("path")
+const fs = require("fs")
+
+const ERROR_ARGS = 1
+const ERROR_APP_EXISTS = 2
+
+// Requires the first command line argument
+if (process.argv.length < 3) {
+  console.error("ERROR: Missing required <my-app>")
+  process.exit(ERROR_ARGS)
+}
+
+// Ensures that the directory doesn't exists
+const appName = process.argv[2]
+const appDir = path.join(process.cwd(), appName)
+
+if (fs.existsSync(appDir)) {
+  console.error(`ERROR: ${appDir} already exists`)
+  process.exit(ERROR_APP_EXISTS)
+}
+
+console.log(appDir)
